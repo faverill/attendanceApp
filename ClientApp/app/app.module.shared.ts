edecdc25e8e771c7a546fa4e8515f8b90c555159
Frank W. Averill <faverill45@att.net>
2017-06-28 17:02:28 -0400

    Chapter1 - First Commit - Beginnings of a menu on top

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component'
+import { HeaderComponent } from './components/header/header.component'
 
 import { HomeComponent } from './components/home/home.component';
 
@@ -10,13 +11,17 @@ export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
-        HomeComponent
+		HomeComponent,
+		HeaderComponent
     ],
     imports: [
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: '**', redirectTo: 'home' }
+            //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+            //{ path: 'home', component: HomeComponent },
+            //{ path: '**', redirectTo: 'home' }
+			{ path: '', redirectTo: 'app', pathMatch: 'full' },
+            { path: 'app', component: AppComponent },
+            { path: '**', redirectTo: 'app' }
         ])
     ]
 };
